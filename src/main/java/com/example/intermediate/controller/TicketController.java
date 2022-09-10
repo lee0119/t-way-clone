e5f6a7af1732e8172c8f19896a6fd69737f87385
lee0119 <candogo91@gmail.com>
2022-09-10 15:33:25 +0900

    plz

--- src/main/java/com/example/intermediate/controller/TicketController.java
+++ src/main/java/com/example/intermediate/controller/TicketController.java
@@ -1,28 +1,26 @@
 package com.example.intermediate.controller;
 
+import com.example.intermediate.dto.request.TicketRequestDto;
 import com.example.intermediate.dto.response.ResponseDto;
 import com.example.intermediate.service.TicketService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-@Slf4j
+
 @RequiredArgsConstructor
 @RestController
 public class TicketController {
 
   private final TicketService ticketService;
 
-
   //여행 생성하기
   @RequestMapping(value = "/ticket", method = RequestMethod.POST)
-  public ResponseDto<?> createTicket(HttpServletRequest request) {
-    return ticketService.createTicket( request);
+  public ResponseDto<?> createTicket(@RequestBody TicketRequestDto requestDto,
+                                     HttpServletRequest request) {
+    return ticketService.createTicket(requestDto, request);
   }
-
 
 //  //여행 목록 조회하기
 //  @RequestMapping(value = "/ticket", method = RequestMethod.GET)
@@ -32,19 +30,22 @@ public class TicketController {
 
   //여행 상세 페이지 조회하기
   @RequestMapping(value = "/ticket", method = RequestMethod.GET)
-  public ResponseDto<?> getTicket() throws IOException, ParseException {
+  public ResponseDto<?> getTicket() throws IOException {
     return ticketService.getTicket();
+
   }
 
 
-
-
-
-
   //여행 삭제하기
-//  @RequestMapping(value = "/ticket/{id}", method = RequestMethod.DELETE)
-//  public ResponseDto<?> deleteTicket(@PathVariable Long id,
-//                                   HttpServletRequest request) {
-//    return ticketService.deleteTicket(id, request);
-//  }
+  @RequestMapping(value = "/ticket/{id}", method = RequestMethod.DELETE)
+  public ResponseDto<?> deleteTicket(@PathVariable Long id,
+                                     HttpServletRequest request) {
+    return ticketService.deleteTicket(id, request);
+  }
+
+  @RequestMapping(value = "/getApi", method = RequestMethod.GET)
+  public ResponseDto<?> getApi() throws IOException {
+    return ticketService.getApi();
+
+  }
 }
